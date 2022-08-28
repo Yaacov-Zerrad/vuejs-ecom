4c2ac5653b95943e1ac0fd99066a327456909f3c
Yaacov-Zerrad <101695734+Yaacov-Zerrad@users.noreply.github.com>
2022-08-28 19:54:32 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import store from '@/store'
+
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+import Success from '../views/Success.vue'
+
 const routes = [
   {
     path: '/',
@@ -14,12 +26,99 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/:category_slug/:product_slug',
+    name: 'Product',
+    component: Product,
+    meta:{
+      title:''
+    }
+  },
+  {
+    path:'/:category_slug',
+    name: 'Category',
+    component: Category,
+    meta:{
+      title:''
+    }
+  },
+  {
+    path:'/cart',
+    name: 'Cart',
+    component: Cart,
+    meta:{
+      title:'cart'
+    }
+  },
+  {
+    path:'/sign-up',
+    name: 'SignUp',
+    component: SignUp,
+    meta:{
+      title:'Sign In'
+    }
+  },
+  {
+    path:'/log-in',
+    name: 'LogIn',
+    component: LogIn,
+    meta:{
+      title:'Log In'
+    }
+  },
+  {
+    path:'/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta:{
+      requireLogin: true,
+      title: 'my account'
+    }
+  },
+  {
+    path:'/cart/success',
+    name: 'Success',
+    component: Success,
+    meta:{
+      requireLogin: true,
+      title: 'success',
+    }
+  },
+  {
+    path:'/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta:{
+      requireLogin: true,
+      title: 'checkout',
+    }
+  },
+  {
+    path:'/search',
+    name: 'Search',
+    component: Search,
   }
+
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+router.beforeEach((to, from, next) => {
+  // verifi si login
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated){
+    next({name:'LogIn', query:{ to: to.path } })
+  } else {
+    next()
+  }
+})
+router.afterEach((to) =>{
+  // name of title
+  if (to.meta.title !== ''){
+  document.title = to.meta.title + ' | ETalit'
+}
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcom to E-talit</p>
+        <p class="subtitle">The best talit store online</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Laster Products</h2>
+      </div>
+
+      <ProductBox
+      class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product"  />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import ProductBox from "@/components/ProductBox";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    ProductBox,
+  },
+  mounted() {
+    this.getLastetProducts();
+
+    document.title = "Home | ETalit";
+  },
+  methods: {
+    async getLastetProducts() {
+      this.$store.commit("setIsLoading", true);
+
+      await axios
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      this.$store.commit("setIsLoading", false);
+    },
+  },
+};
 </script>
+
+<style>
+</style>
