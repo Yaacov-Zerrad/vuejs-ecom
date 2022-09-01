40443fbbad3f46eda640a89acc13f36d5d6c04b8
Yaacov-Zerrad <101695734+Yaacov-Zerrad@users.noreply.github.com>
2022-09-01 15:17:01 +0300

    change url

--- src/components/ProductBox.vue
+++ src/components/ProductBox.vue
@@ -2,7 +2,7 @@
 <div class="column is-3">
       <div class="box">
         <figure class="image mb-4">
-          <img :src="product.get_thumbnail">
+          <img :src="product.thumbnail">
         </figure>
 
         <h3 class="is-size-4">{{ product.name }}</h3>
