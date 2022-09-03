d06dfad78236a613aa0b6fd6641aea29bf0e02be
yaacov zerrad <yaazerrad@gmail.com>
2022-09-04 00:59:11 +0300

    add change allergy

--- src/router/index.js
+++ src/router/index.js
@@ -22,14 +22,6 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
     path:'/:category_slug/:product_slug',
     name: 'Product',
     component: Product,
